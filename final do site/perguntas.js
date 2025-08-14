8e798036122e3b97106ab9d23f416a0a62de13da
kely2009 <kelymara.volff@escola.pr.gov.br>
2025-08-14 11:47:35 -0300

    Add files via upload

---
+++ final do site/perguntas.js
@@ -0,0 +1,35 @@
+
+criaCartao(
+    'biologia',
+    'Como é a pele dos réptes?',
+    'A pele é seca, resistente e impermeavel.'
+)
+
+criaCartao(
+   'biologia',
+    'Qual a primeira classe a alcansar o ambiente terrestre?',
+    'Classe reptilia.'
+)
+
+criaCartao(
+    'biologia',
+    'Em quantas classe os réptes são divididos?',
+    'Eles são divididos em 6 classe.'
+)
+
+criaCartao(
+    'biologia',
+    'O que são aves ratitas?',
+    'São aves que não voadoras'
+)
+
+criaCartao(
+    'biologia',
+    'Por que as aves  carenetas podem voar?',
+    'Porque elas possuem quintilha e musculatura bem desenvolvidas'
+)
+criaCartao(
+    'biologia',
+    'quantas espécies  de animais mamíferos foi descobertas até hoje?',
+    'Foram descobertas até hoje 5,4 mil espécies no mundo.'
+)
